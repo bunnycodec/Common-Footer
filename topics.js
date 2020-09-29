1af064b870cdf93984d47de6e9e06d10981e27b9
Sunny Kumar <sunnykumar@bunnycodec.com>
2020-09-29 22:56:21 +0530

    Initial Commit

---
+++ topics.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+
+  // Transitoion --Sunny Starts
+
+  $(".footer-icons span").mouseover(function () {
+    $(this).addClass('fa-inverse');
+    $(this).mouseout(function () {
+      $(this).removeClass('fa-inverse');
+    });
+  });
+
+});
+
+
+
+
